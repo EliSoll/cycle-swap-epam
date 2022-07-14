b90967f18e17dd7de133bd96f71ddef85483f1e6
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-07-14 18:18:11 +0300

    teset1

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,22 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        if(array.length > 0) {
+            int temporaryNum = array[array.length - 1];
+            System.arraycopy(array, 0, array, 1, array.length - 1);
+        array[0] = temporaryNum;
+        }
     }
 
-    static void cycleSwap(int[] array, int shift) {
 
-    }
+
+        static void cycleSwap(int[] array, int shift) {
+        if(array.length > 0) {
+                for(int j = 0; j < shift; j++) {
+                    int temporaryNum = array[array.length - 1];
+                    System.arraycopy(array, 0, array, 1, array.length - 1);
+                    array[0] = temporaryNum;
+                }
+                }
+            }
 }
